987b423a4f37d53006efefdc8f903ba7a9a2417c
VikashSurin <vikashsurin10@gmail.com>
2022-04-01 14:14:50 +0530

    initial commit

---
+++ pages/bye/index.tsx
@@ -0,0 +1,7 @@
+export default function Bye() {
+  return (
+    <div>
+      <p>Hello world!</p>
+    </div>
+  );
+}
---
+++ pages/hi/index.tsx
@@ -0,0 +1,7 @@
+export default function Hi() {
+  return (
+    <div>
+      <p>Hello world!</p>
+    </div>
+  );
+}
